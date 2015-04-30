2cffa016407721540b800cf9af1921764bb5f057
Nico Schlömer <nico.schloemer@gmail.com>
2015-04-30 16:54:51 +0200

    proper content fetching from background

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -5,23 +5,18 @@ var angular = require('angular');
 var paperhive = angular.module('paperHive', []);
 
 paperhive.controller('PopupCtrl', ['$scope', function($scope) {
-  $scope.test = '123';
+  var bg = chrome.extension.getBackgroundPage();
+  $scope.article = {};
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      // for some reason, we need $apply here
+      $scope.$apply(function() {
+        $scope.article.meta = bg.tabToArticle[tabs[0].id];
+        $scope.article.discussions = bg.tabToDiscussions[tabs[0].id];
+      });
+    });
 }]);
-
-var bg = chrome.extension.getBackgroundPage();
-
-chrome.tabs.query(
-  {
-    active: true,
-    currentWindow: true
-  },
-  function(tabs) {
-  // fetch meta info from active tab
-  var article = bg.tabToArticle[tabs[0].id];
-
-  document.getElementById('title').textContent = article.title;
-  document.getElementById('authors').textContent = article.authors.join(', ');
-  document.getElementById('link').href =
-    'https://paperhive.org/dev/frontend/branches/master/#/articles/' +
-    article._id;
-});
