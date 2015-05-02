27c950c3370a367b49a1ca5abbb3e5746363afee
Nico Schlömer <nico.schloemer@gmail.com>
2015-05-02 15:51:42 +0200

    display number of discussions at a PDF

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -8,6 +8,17 @@
       if (request.discussions) {
         var elemDiv = document.createElement('div');
         elemDiv.className = 'ph-notification-box';
+        var text;
+        if (request.discussions.length === 1) {
+          text = 'There is <strong>1</strong> discussion for this PDF.';
+        } else if (request.discussions.length > 1) {
+          text = 'There are <strong>' + request.discussions.length +
+            '</strong> discussions for this PDF.';
+        }
+        //var content = document.createTextNode(text);
+        //elemDiv.appendChild(content);
+        elemDiv.innerHTML = elemDiv.innerHTML + text;
+
         document.body.appendChild(elemDiv);
       }
     });
