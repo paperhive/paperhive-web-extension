a0591b758d80a8491964cfd156807d3c31a6eec1
Nico Schlömer <nico.schloemer@gmail.com>
2015-05-08 00:35:31 +0200

    reactivate checksum matching for PDFs

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -4,18 +4,9 @@
   var async = require('async');
   var config = require('../../config.json');
 
-  //// from <http://stackoverflow.com/a/21042958/353337>
-  //var extractHeader = function(headers, headerName) {
-  //  for (var i = 0; i < headers.length; ++i) {
-  //    var header = headers[i];
-  //    if (header.name.toLowerCase() === headerName) {
-  //      return header;
-  //    }
-  //  }
-  //};
-
   var tabToArticle = {};
   var tabToDiscussions = {};
+  var tabToMimeType = {};
   var responseSender = {};
 
   chrome.webRequest.onBeforeRequest.addListener(
@@ -34,7 +25,6 @@
             if (config.whitelistedHostnames.indexOf(parser.hostname) < 0) {
               callback(null, undefined);
             }
-            console.log(123);
 
             var xhr = new XMLHttpRequest();
             xhr.open(
@@ -117,141 +107,123 @@
     isColor[tabId] = true;
   };
 
-  var getArticle = function(url) {
-    // We could actually check on every single page, but we don't want to put
-    // the PaperHive backend under too much load. Hence, filter by hostname.
-    // URL parsing in JS: <https://gist.github.com/jlong/2428561>
-    var parser = document.createElement('a');
-    parser.href = url;
-    if (config.whitelistedHostnames.indexOf(parser.hostname) < 0) {
-      return undefined;
-    }
-
-    console.log(444);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', config.apiUrl + '/articles/sources?handle=' + url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      console.log(this);
-      if (this.status === 200) {
-        console.log(555);
-        return this.response;
-      } else {
-        console.log(666);
-        return undefined;
+  // from <http://stackoverflow.com/a/21042958/353337>
+  var extractHeader = function(headers, headerName) {
+    for (var i = 0; i < headers.length; ++i) {
+      var header = headers[i];
+      if (header.name.toLowerCase() === headerName) {
+        return header;
       }
-    };
-    xhr.send(null);
+    }
   };
 
   chrome.webRequest.onHeadersReceived.addListener(
     function(details) {
+      if (!tabToArticle[details.tabId] && details.tabId >= 0) {
+        var header = extractHeader(
+          details.responseHeaders,
+          'content-type'
+        );
+        // If the header is set, use its value. Otherwise, use undefined.
+        tabToMimeType[details.tabId] =
+          header && header.value.split(';', 1)[0];
+      }
     },
     {
-      urls: ['*://*/*'],
+      urls: ['*://*/*.pdf'],
       types: ['main_frame']
     },
     ['responseHeaders']
   );
-
-  var getPdfHash = function(url, callback) {
-    // Since we have no access to the PDF data, we have to
-    // fetch it again and hope it gets served from cache.
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function() {
-      if (this.status === 200) {
-        // read the blob data, cf.
-        // <http://www.html5rocks.com/en/tutorials/file/xhr2/>
-        var a = new FileReader();
-        a.readAsBinaryString(this.response);
-        a.onloadend = function() {
-          var hash = crypto.createHash('sha1');
-          hash.update(a.result, 'binary');
-          callback(null, hash.digest('hex'));
-        };
-      } else {
-        callback('Could not fetch PDF.');
-      }
-    };
-    xhr.send(null);
-  };
-
-  var checkBySha = function(hash, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', config.apiUrl + '/articles/bySha/' + hash, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      if (this.status === 200) {
-        tabToArticle[details.tabId] = xhr.response;
-        // Set the icon to color.
-        // This might have already been done above, we need to do it
-        // here to account for PDFs which are in our system but the
-        // host which serves it is not actually approved. This happens,
-        // for example, if someone copies an arXiv article to another
-        // server.
-        if (!isColor[details.tabId]) {
-          setColorIcon(details.tabId);
-        }
-        callback(null, xhr.response);
-      } else if (this.status === 404) {
-        callback('PDF not found on PaperHive');
-      } else {
-        callback('Unexpected return value');
-      }
-    };
-    xhr.send(null);
-  };
-
-  var fetchDiscussions = function(article, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(
-      'GET',
-      config.apiUrl + '/articles/' + article._id + '/discussions/',
-      true
-    );
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      if (this.status === 200) {
-        tabToDiscussions[details.tabId] = xhr.response;
-        callback(null, article, xhr.response);
-      } else {
-        callback('Unexpected return value');
-      }
-    };
-    xhr.send(null);
-  };
 
   // Chrome 42 doesn't properly fire chrome.webRequest.onCompleted/main_frame
   // when loading a PDF page. When it's served from cache, it does.
   // See <https://code.google.com/p/chromium/issues/detail?id=481411>.
   chrome.webRequest.onCompleted.addListener(
     function(details) {
-      //console.log('asda');
-      //console.log(tabToArticle[details.tabId]);
-      //if (tabToArticle[details.tabId]) {
-      //  tabToDiscussions[details.tabId] = null;
+      if (!tabToArticle[details.tabId]) {
+        tabToDiscussions[details.tabId] = null;
 
-      //  async.waterfall([
-      //    getPdfHash,
-      //    checkBySha,
-      //    fetchDiscussions
-      //  ],
-      //  function(err, article, discussions) {
-      //    // make the loading as complete
-      //    // send a response if so required
-      //    if (responseSender[details.tabId]) {
-      //      responseSender[details.tabId]({
-      //        article: article,
-      //        discussions: discussions
-      //      });
-      //      responseSender[details.tabId] = null;
-      //    }
-      //  }
-      //  );
-      //}
+        async.waterfall([
+          function getPdfHash(callback) {
+            // Since we have no access to the PDF data, we have to
+            // fetch it again and hope it gets served from cache.
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', details.url, true);
+            xhr.responseType = 'blob';
+            xhr.onload = function() {
+              if (this.status === 200) {
+                // read the blob data, cf.
+                // <http://www.html5rocks.com/en/tutorials/file/xhr2/>
+                var a = new FileReader();
+                a.readAsBinaryString(this.response);
+                a.onloadend = function() {
+                  var hash = crypto.createHash('sha1');
+                  hash.update(a.result, 'binary');
+                  callback(null, hash.digest('hex'));
+                };
+              } else {
+                callback('Could not fetch PDF.');
+              }
+            };
+            xhr.send(null);
+          },
+          function checkBySha(hash, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', config.apiUrl + '/articles/bySha/' + hash, true);
+            xhr.responseType = 'json';
+            xhr.onload = function() {
+              if (this.status === 200) {
+                tabToArticle[details.tabId] = xhr.response;
+                // Set the icon to color.
+                // This might have already been done above, we need to do it
+                // here to account for PDFs which are in our system but the
+                // host which serves it is not actually approved. This happens,
+                // for example, if someone copies an arXiv article to another
+                // server.
+                chrome.pageAction.show(details.tabId);
+                setColorIcon(details.tabId);
+                callback(null, xhr.response);
+              } else if (this.status === 404) {
+                callback('PDF not found on PaperHive');
+              } else {
+                callback('Unexpected return value');
+              }
+            };
+            xhr.send(null);
+          },
+          function fetchDiscussions(article, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(
+              'GET',
+              config.apiUrl + '/articles/' + article._id + '/discussions/',
+              true
+            );
+            xhr.responseType = 'json';
+            xhr.onload = function() {
+              if (this.status === 200) {
+                tabToDiscussions[details.tabId] = xhr.response;
+                callback(null, article, xhr.response);
+              } else {
+                callback('Unexpected return value');
+              }
+            };
+            xhr.send(null);
+          }
+        ],
+        function(err, article, discussions) {
+          // make the loading as complete
+          // send a response if so required
+          if (responseSender[details.tabId]) {
+            responseSender[details.tabId]({
+              article: article,
+              discussions: discussions
+            });
+            responseSender[details.tabId] = null;
+          }
+        }
+        );
+      }
     },
     {
       urls: ['*://*/*'],
